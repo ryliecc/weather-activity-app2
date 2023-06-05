45474a8fb1b218ade2a91f4a0f65248ba9323286
Rylie Castell <rylie.castell@gmail.com>
2023-06-05 18:48:03 +0200

    add handleAddActivity

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import "./App.css";
+import { useState } from "react";
+import { uid } from "uid";
 import Weather from "./components/Weather";
 import ActivityForm from "./components/ActivityForm";
 import ActivityList from "./components/ActivityList";
 
 export default function App() {
+  const [allActivities, setAllActivities] = useState([]);
+
+  function handleAddActivity(event) {
+    event.preventDefault();
+    const form = event.target;
+    const newActivity = {
+      id: uid(),
+      name: form.name.value,
+      isForGoodWeather: form.elements.isSunny.checked,
+      isForBadWeather: form.elements.isRainy.checked,
+    };
+    setAllActivities([...allActivities, newActivity]);
+    console.log(allActivities);
+    form.reset();
+    form.elements.name.focus();
+  }
+
   return (
     <>
       <Weather>
         <ActivityList />
       </Weather>
-      <ActivityForm />
+      <ActivityForm onAddActivity={handleAddActivity} />
     </>
   );
 }
