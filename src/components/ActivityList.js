1231d2bc5b176e24900a85454d58f12292d783fb
Rylie Castell <rylie.castell@gmail.com>
2023-06-05 21:10:44 +0200

    add delete button

--- src/components/ActivityList.js
+++ src/components/ActivityList.js
@@ -1,11 +1,16 @@
-export default function ActivityList({ displayedActivities }) {
+export default function ActivityList({
+  displayedActivities,
+  onDeleteActivity,
+}) {
   return (
     <ul className="activity-list">
       {displayedActivities.map((activity) => {
         return (
           <li className="activity-list-item" key={activity.id}>
             {activity.name}
-            <button type="button">❌</button>
+            <button type="button" onClick={() => onDeleteActivity(activity.id)}>
+              ❌
+            </button>
           </li>
         );
       })}
